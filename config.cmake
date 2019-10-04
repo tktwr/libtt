8086c0f960955075187bb40df5a5e8f5f0b78bc6
tktwr <takehiro.tawara@gmail.com>
2019-10-05 00:48:54 +0900

    update OPT_DIR

--- config.cmake
+++ config.cmake
@@ -58,13 +58,21 @@ set(LIBTT ${CMAKE_CURRENT_LIST_DIR})
 
 include(${LIBTT}/cmake/libtt.cmake)
 
-if(EXISTS ${LIBTT}/local.cmake)
-    include(${LIBTT}/local.cmake)
-endif()
-
 #-------------------------------------------------
 # third_party
 #-------------------------------------------------
+
+set(OPT_DIR Z:/opt)
+
+if(ANDROID)
+    set(OpenCV_DIR Z:/opt/opencv-4.1.0-android/sdk/native/jni)
+elseif(MSVC)
+    set(OpenCV_DIR Z:/opt/opencv-4.1.0/build)
+endif()
+
+if(EXISTS ${LIBTT}/local.cmake)
+    include(${LIBTT}/local.cmake)
+endif()
 
 include(${LIBTT}/cmake/glad.cmake)
 include(${LIBTT}/cmake/eigen.cmake)
